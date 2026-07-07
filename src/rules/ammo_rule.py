7734137d29e632f640149243a286bdee53b4a3be
abolfazl <a.asghari251@gmail.com>
2026-07-07 14:56:54 +0330

    feat: Impliment ammo_rule

---
+++ src/rules/ammo_rule.py
@@ -0,0 +1,14 @@
+from src.entities.weapon import Weapon
+from src.entities.target import Target
+from src.entities.rule_result import RuleResult
+from src.rules.base_rule import BaseRule
+
+class AmmoRule(BaseRule):
+    def check(self, weapon: Weapon, target: Target) -> RuleResult:
+        if weapon.ammo > 0:
+            return RuleResult(rule_name="AmmoRule", 
+                              passed=True)
+        
+        return RuleResult(rule_name="StatusRule", 
+                          passed=False,
+                          reason=("ammunition"))
