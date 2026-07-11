8f5eb582e1ece27626b502e0e4ce4610470ad524
abolfazl asghari <abolfazl@localhost.localdomain>
2026-07-11 09:04:02 +0330

    feat: Impliment range rule

---
+++ test/test_assignment_validator.py
@@ -0,0 +1,9 @@
+import unittest
+from src.rules.base_rule import BaseRule
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_something(self):
+        pass    
