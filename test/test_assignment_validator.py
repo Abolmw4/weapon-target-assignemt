c264d990bc7fcecf3813c76c8be8395738f3ce04
Abolfazl Asghari <abolfazl@localhost.localdomain>
2026-07-11 11:29:44 +0330

    test: Test implimented rule

--- test/test_assignment_validator.py
+++ test/test_assignment_validator.py
@@ -14,20 +14,73 @@ from src.validator.assignment_validator import AssignmentValidator
 from src.entities.weapon import Weapon
 from src.entities.target import Target
 from src.entities.assignment import Assignment
+from src.entities.validation_report import ValidationReport
 from typing import List, Dict
-
+from copy import deepcopy
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.weapons: List[Weapon] = WeapnRepository(file_path="/home/abolfazl/Documents/weapon-target-assignemt/data/weapon.json").load()
-        self.target: List[Target] = TargetRepository(file_path="/home/abolfazl/Documents/weapon-target-assignemt/data/target.json").load()
+        self.targets: List[Target] = TargetRepository(file_path="/home/abolfazl/Documents/weapon-target-assignemt/data/target.json").load()
         self.compatibiltiy: Dict[str, List[str]] = CompatibilityRepository(file_path="/home/abolfazl/Documents/weapon-target-assignemt/data/compatibility.json").load()
-        self.validator = AssignmentValidator(rules=[StatusRule(), AmmoRule(), RangeRule(), CompatiblityRule(CompatibilityRepository(file_path="/home/abolfazl/Documents/weapon-target-assignemt/data/compatibility.json"))])
+        
         
     def test_valid_assignment(self):
-
+        validator = AssignmentValidator(rules=[StatusRule()])
         weapon = self.weapons[0]
+        target = self.targets[0]
+
+        report = validator.validate(weapon, target, assignment=Assignment(assignment_id=1, weapon_id=weapon.id, target_id=target.id))
+
+        self.assertTrue(report.valid)
+    
+    def test_invalid_status(self):
+        validator = AssignmentValidator(rules=[StatusRule()])
+        
+        weapon = self.weapons[0]
+
         weapon.status = WeaponStatus.RELOADING
-        target = self.target[0]
-        report = self.validator.validate(weapon=weapon, target=target, assignment=Assignment(weapon_id=1, target_id=1, assignment_id=1))
+
+        target = self.targets[0]
+
+        report = validator.validate(weapon, target, assignment=Assignment(assignment_id=1, weapon_id=weapon.id, target_id=target.id))
+
+        self.assertFalse(report.valid)
+
+    def test_no_ammo(self):
+        validator = AssignmentValidator(rules=[StatusRule(), AmmoRule()])
+        
+        weapon = self.weapons[0]
+
+        weapon.ammo = 0
+
+        target = self.targets[0]
+
+        report = validator.validate(weapon, target, assignment=Assignment(assignment_id=1, weapon_id=weapon.id, target_id=target.id))
+
+        self.assertFalse(report.valid)
+        print(report.result[0].reason)
+        
+    def test_incompatible_weapon_target(self):
+        validator = AssignmentValidator(rules=[StatusRule(), AmmoRule(), CompatiblityRule(CompatibilityRepository(file_path="/home/abolfazl/Documents/weapon-target-assignemt/data/compatibility.json"))])
+        weapon = deepcopy(self.weapons[3])      # AAA
+        target = deepcopy(self.targets[1])      # BALISTIC MISSILE
+
+        report: ValidationReport = validator.validate(weapon=weapon, target=target, assignment=Assignment(assignment_id=1, weapon_id=weapon.id, target_id=target.id))
+
+        self.assertFalse(report.valid)
+
+
+
+    def test_target_out_of_range(self):
+        validator = AssignmentValidator(rules=[StatusRule(), AmmoRule(), CompatiblityRule(CompatibilityRepository(file_path="/home/abolfazl/Documents/weapon-target-assignemt/data/compatibility.json")), RangeRule()])
+
+        weapon = deepcopy(self.weapons[0])
+
+        target = deepcopy(self.targets[0])
+
+        # target.position = (40.000000, 58.000000, 1000)
+
+        report = validator.validate(weapon=weapon, target=target, assignment=Assignment(assignment_id=1, weapon_id=weapon.id, target_id=target.id))
+
         self.assertFalse(report.valid)
